bab7dc5ee4490e900681fc761f13acacf868601d
sumankmaiti <sumankmaiti01@gmail.com>
2023-05-06 22:48:54 +0530

    widget

--- frontend/src/components/widget/Widget.jsx
+++ frontend/src/components/widget/Widget.jsx
@@ -1,10 +1,23 @@
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
 import './widget.scss'
 
 const Widget = () => {
   return (
     <div className='widget'>
-        <div className='left'>left</div>
-        <div className='right'>right</div>
+        <div className='left'>
+          <span className='title'> USERS </span>
+          <span className='counter'> 212545 </span>
+          <span className='link'> See all users </span>
+        </div>
+        <div className='right'>
+          <div className="percentage positive">
+            <KeyboardArrowUpOutlinedIcon />
+            20%
+          </div>
+          <PersonOutlinedIcon className='icon' />
+        </div>
     </div>
   )
 }
