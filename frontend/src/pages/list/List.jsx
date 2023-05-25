1559e4a6c0ad42d36c6201b7bda36b666a3797fb
sumankmaiti <sumankmaiti01@gmail.com>
2023-05-25 13:08:12 +0530

    data table completed

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,17 +1,18 @@
-import Navbar from'../../components/navbar/Navbar'
+import Datatable from '../../components/datatable/Datatable'
+import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import "./list.scss"
 
 const List = () => {
-  return (
-	<div className='list'>
-    <Sidebar />
-    <div className="listContainer">
-      <Navbar />
-      datatable
-    </div>
-  </div>
-  )
+	return (
+		<div className='list'>
+			<Sidebar/>
+			<div className="listContainer">
+				<Navbar/>
+				<Datatable/>
+			</div>
+		</div>
+	)
 }
 
 export default List
