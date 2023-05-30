5f76ace1eccaf7529e57ba026abb739f4d721916
sumankmaiti <sumankmaiti01@gmail.com>
2023-05-30 12:52:45 +0530

    login going

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "reactadminpanel-e0268.firebaseapp.com",
+  projectId: "reactadminpanel-e0268",
+  storageBucket: "reactadminpanel-e0268.appspot.com",
+  messagingSenderId: "725116414414",
+  appId: "1:725116414414:web:5114db84f129d053f5e39f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,39 @@
+import { useState } from 'react'
 import './login.scss'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase';
 
 const Login = () => {
-  return (
-	<div>Login</div>
-  )
+	const [error, setError] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const loginHandler = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in 
+				const user = userCredential.user;
+				// ...
+				setError("")
+				console.log(user);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				setError(errorMessage)
+			});
+	}
+
+	return (
+		<div className='login'>
+			<form action={loginHandler}>
+				<input type="email" placeholder='Email' onChange={e => setEmail(e.target.value)} />
+				<input type="password" placeholder='Password' onChange={e => setPassword(e.target.value)} />
+				<button type='submit'> Login </button>
+				{error && <span>{error}</span>}
+			</form>
+		</div>
+	)
 }
 
 export default Login
