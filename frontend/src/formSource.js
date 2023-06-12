7ccddb663bb323b10f46f91125dcda4168fb639a
sumankmaiti <sumankmaiti01@gmail.com>
2023-06-12 18:00:56 +0530

    file upload

--- frontend/src/formSource.js
+++ frontend/src/formSource.js
@@ -1,41 +1,41 @@
 export const userInputs = [
     {
-      id: 1,
+      id: "username",
       label: "Username",
       type: "text",
       placeholder: "suman_maiti",
     },
     {
-      id: 2,
+      id: "fullName",
       label: "Name and surname",
       type: "text",
       placeholder: "Suman Maiti",
     },
     {
-      id: 3,
+      id: "email",
       label: "Email",
       type: "mail",
       placeholder: "sumanmaiti@gmail.com",
     },
     {
-      id: 4,
+      id: "phone",
       label: "Phone",
       type: "text",
       placeholder: "+91 0123456789",
     },
     {
-      id: 5,
+      id: "password",
       label: "Password",
       type: "password",
     },
     {
-      id: 6,
+      id: "address",
       label: "Address",
       type: "text",
       placeholder: "Elton St. 216 NewYork",
     },
     {
-      id: 7,
+      id: "country",
       label: "Country",
       type: "text",
       placeholder: "USA",
@@ -44,31 +44,31 @@ export const userInputs = [
   
   export const productInputs = [
     {
-      id: 1,
+      id: "title",
       label: "Title",
       type: "text",
       placeholder: "Apple Macbook Pro",
     },
     {
-      id: 2,
+      id: "description",
       label: "Description",
       type: "text",
       placeholder: "Description",
     },
     {
-      id: 3,
+      id: "category",
       label: "Category",
       type: "text",
       placeholder: "Computers",
     },
     {
-      id: 4,
+      id: "price",
       label: "Price",
       type: "text",
       placeholder: "100",
     },
     {
-      id: 5,
+      id: "stock",
       label: "Stock",
       type: "text",
       placeholder: "in stock",
--- frontend/src/pages/new/New.jsx
+++ frontend/src/pages/new/New.jsx
@@ -1,114 +1,112 @@
 import UploadFileIcon from "@mui/icons-material/UploadFile";
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore"; 
+import { useEffect, useState } from "react";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./new.scss";
-import { db } from "../../firebase";
+import { db, auth, storage } from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const New = ({ input, title }) => {
-  const [file, setFile] = useState("")
-  const [data, setData] = useState({})
+	const [file, setFile] = useState("")
+	const [data, setData] = useState({})
 
-  const handleData = (e) => {
-	const id = e.target.id
-	const value = e.target.value
-	setData({...data, [id]: value})
-  }
+	useEffect(() => {
+		const name = new Date().getTime() + file.name
+		const spaceRef = ref(storage, name);
+		const uploadTask = uploadBytesResumable(spaceRef, file);
 
-  const handleAdd = async (e) => {
-	e.preventDefault()
-	try {
-		const res = await addDoc(collection(db, "cities"), {
-			name: "Los Angeles",
-			state: "CA",
-			country: "USA",
-			timeStamp: serverTimestamp()
-		  }); 
-		console.log(res.id);
+		uploadTask.on('state_changed',
+			(snapshot) => {
+				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				console.log('Upload is ' + progress + '% done');
+				switch (snapshot.state) {
+					case 'paused':
+						console.log('Upload is paused');
+						break;
+					case 'running':
+						console.log('Upload is running');
+						break;
+					default:
+						break
+				}
+			},
+			(error) => {
+				console.log(error);
+			},
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					setData((prev) => ({...prev, img: downloadURL}))
+				});
+			}
+		);
+	}, [file])
+
+	const handleData = (e) => {
+		const id = e.target.id
+		const value = e.target.value
+		setData({ ...data, [id]: value })
 	}
-	catch(err) {
-		console.log(err);
+
+	console.log(data);
+
+	const handleAdd = async (e) => {
+		e.preventDefault()
+
+		try {
+			const res = await createUserWithEmailAndPassword(auth, data.email, data.password)
+			await setDoc(doc(db, "users", res.user.uid), {
+				...data,
+				timeStamp: serverTimestamp()
+			});
+		}
+		catch (err) {
+			console.log(err);
+		}
+
 	}
-	
-  }
 
-  return (
-    <div className="new">
-      <Sidebar />
-      <div className="newContainer">
-        <Navbar />
-        <div className="top">
-          <h1>{title}</h1>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <img
-              src= {file ? URL.createObjectURL(file) : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"}
-              alt=""
-            />
-          </div>
-          <div className="right">
-            <form onSubmit={handleAdd}>
-              <div className="formInput">
-                <label htmlFor="file">
-                  Image: <UploadFileIcon className="icon" />
-                </label>
-                <input onChange={e=>setFile(e.target.files[0])} type="file" id="file" style={{ display: "none" }} />
-              </div>
+	return (
+		<div className="new">
+			<Sidebar />
+			<div className="newContainer">
+				<Navbar />
+				<div className="top">
+					<h1>{title}</h1>
+				</div>
+				<div className="bottom">
+					<div className="left">
+						<img
+							src={file ? URL.createObjectURL(file) : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"}
+							alt=""
+						/>
+					</div>
+					<div className="right">
+						<form onSubmit={handleAdd}>
+							<div className="formInput">
+								<label htmlFor="file">
+									Image: <UploadFileIcon className="icon" />
+								</label>
+								<input onChange={e => setFile(e.target.files[0])} type="file" id="file" style={{ display: "none" }} />
+							</div>
 
-              {input.map((item) => {
-                return (
-                  <div className="formInput" key={item.id}>
-                    <label>{item.label}</label>
-                    <input type={item.type} placeholder={item.placeholder} onChange={handleData} />
-                  </div>
-                );
-              })}
-			  <button type="submit">Send</button>
-              {/* <div className="formInput">
-                <label>Usename</label>
-                <input type="text" placeholder="suman_maiti" />
-              </div>
-
-              <div className="formInput">
-                <label>Full Name</label>
-                <input type="text" placeholder="suman maiti" />
-              </div>
-
-              <div className="formInput">
-                <label>Email</label>
-                <input type="email" placeholder="sumanmaiti@gmail.com" />
-              </div>
-
-              <div className="formInput">
-                <label>Phone</label>
-                <input type="text" placeholder="12345678" />
-              </div>
-
-              <div className="formInput">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-
-              <div className="formInput">
-                <label>Address</label>
-                <input type="text" placeholder="dum dm road, kolkata, wb" />
-              </div>
-
-              <div className="formInput">
-                <label>Country</label>
-                <input type="text" placeholder="India" />
-              </div> */}
-
-              {/* <button>Send</button> */}
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							{input.map((item) => {
+								return (
+									<div className="formInput" key={item.id}>
+										<label>{item.label}</label>
+										<input id={item.id} type={item.type} placeholder={item.placeholder} onChange={handleData} />
+									</div>
+								);
+							})}
+							<button type="submit">Send</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default New;
